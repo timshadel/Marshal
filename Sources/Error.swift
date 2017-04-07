f1384bbe5f748e15583b9165761b7eeb1569472b
Tim Shadel <tim@shadelsoftware.com>
2017-04-07 14:36:34 -0600

    Add error description

--- Sources/Error.swift
+++ Sources/Error.swift
@@ -14,13 +14,13 @@
 import Foundation
 
 
-public enum MarshalError: Error, CustomStringConvertible {
+public enum MarshalError: LocalizedError, CustomStringConvertible {
     
     case keyNotFound(key: KeyType)
     case nullValue(key: KeyType)
     case typeMismatch(expected: Any, actual: Any)
     case typeMismatchWithKey(key: KeyType, expected: Any, actual: Any)
-    
+
     public var description: String {
         switch self {
         case let .keyNotFound(key):
@@ -32,5 +32,9 @@ public enum MarshalError: Error, CustomStringConvertible {
         case let .typeMismatchWithKey(key, expected, actual):
             return "Type mismatch. Expected type \(expected) for key: \(key.stringValue). Got '\(actual)'"
         }
+    }
+
+    public var errorDescription: String? {
+        return description
     }
 }
